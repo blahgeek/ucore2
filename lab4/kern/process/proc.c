f9f3c20589c098edde7f5b6f848eb4054b5076ca
BlahGeek <blahgeek@gmail.com>
2014-03-25 13:55:31 +0800

    proc_init seems works, swap assertion failed

--- lab4/kern/process/proc.c
+++ lab4/kern/process/proc.c
@@ -86,6 +86,18 @@ static struct proc_struct *
 alloc_proc(void) {
     struct proc_struct *proc = kmalloc(sizeof(struct proc_struct));
     if (proc != NULL) {
+        proc->state = PROC_UNINIT;
+        proc->pid = -1;
+        proc->runs = 0;
+        proc->kstack = 0;
+        proc->need_resched = 0;
+        proc->parent = current;
+        proc->mm = NULL;
+        memset(&(proc->context), 0, sizeof(struct context));
+        proc->tf = NULL;
+        proc->cr3 = boot_cr3;
+        proc->flags = 0;
+        set_proc_name(proc, "NONAME");
     //LAB4:EXERCISE1 YOUR CODE
     /*
      * below fields in proc_struct need to be initialized
@@ -290,12 +302,25 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
      */
 
     //    1. call alloc_proc to allocate a proc_struct
+    proc = alloc_proc();
+    if(proc == NULL)
+        goto fork_out;
     //    2. call setup_kstack to allocate a kernel stack for child process
+    if(setup_kstack(proc) != 0)
+        goto bad_fork_cleanup_proc;
     //    3. call copy_mm to dup OR share mm according clone_flag
+    if(copy_mm(clone_flags, proc) != 0)
+        goto bad_fork_cleanup_kstack;
+    proc->pid = get_pid();
     //    4. call copy_thread to setup tf & context in proc_struct
+    copy_thread(proc, stack, tf);
     //    5. insert proc_struct into hash_list && proc_list
-    //    6. call wakup_proc to make the new child process RUNNABLE
+    hash_proc(proc);
+    list_add(&proc_list, &(proc->list_link));
+    //    6. call wakeup_proc to make the new child process RUNNABLE
+    wakeup_proc(proc);
     //    7. set ret vaule using child proc's pid
+    ret = proc->pid;
 fork_out:
     return ret;
 
